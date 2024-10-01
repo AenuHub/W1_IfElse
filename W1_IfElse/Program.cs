9b26aebbca9fec44654912d4f80d723fe021e88e
AenuHub <jesuskane92@gmail.com>
2024-10-01 10:11:47 +0300

    Add project files.

---
+++ W1_IfElse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace W1_IfElse
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input > 10)
+            {
+                Console.WriteLine("Girilen sayı 10'dan buyuktur.");
+            } else if (input < 10) {
+                Console.WriteLine("Girilen sayı 10'dan kucuktur.");
+            } else {
+                Console.WriteLine("Girilen sayı 10'a esittir.");
+            }
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("Girilen sayı cifttir.");
+            } else {
+                Console.WriteLine("Girilen sayı tektir.");
+            }
+        }
+    }
+}
